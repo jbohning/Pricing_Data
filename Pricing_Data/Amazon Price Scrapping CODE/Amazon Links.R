86595dd688bbde46db331873010e277638bcc1c1
jbohning <jsh1818@yahoo.com>
2017-03-03 14:23:58 -0600

    Add files via upload

---
+++ Pricing_Data/Amazon Price Scrapping CODE/Amazon Links.R
@@ -0,0 +1,75 @@
+#List of urls of products from the following link: https://www.amazon.com/s/ref=sr_hi_5?rh=n%3A172282%2Cn%3A541966%2Cn%3A193870011%2Cn%3A284822%2Cp_n_feature_keywords_five_browse-bin%3A6147187011%2Ck%3Agtx+1060&keywords=gtx+1060&ie=UTF8&qid=1471649465
+
+#PRIME LIST
+producturls<-c("https://www.amazon.com/EVGA-GeForce-Support-Graphics-08G-P4-6173-KR/dp/B01GX5YWAO/ref=sr_1_1?s=pc&ie=UTF8&qid=1488550816&sr=1-1&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/MSI-GAMING-GTX-1070-8G/dp/B01GXOX3SW/ref=sr_1_4?s=pc&ie=UTF8&qid=1488550816&sr=1-4&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/Gigabyte-GeForce-Graphics-GV-N1070G1-GAMING-8GD/dp/B01H0WU884/ref=sr_1_5?s=pc&ie=UTF8&qid=1488550816&sr=1-5&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/ZOTAC-ZT-P10700C-10P-Wraparound-Ultra-wide-PowerBoost/dp/B01GVNNOSY/ref=sr_1_6?s=pc&ie=UTF8&qid=1488550816&sr=1-6&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/ASUS-GeForce-STRIX-Graphics-STRIX-GTX1080-8G-GAMING/dp/B01GRRRW0A/ref=sr_1_7?s=pc&ie=UTF8&qid=1488550816&sr=1-7&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/NVIDIA-GEFORCE-GTX-1080-Founders/dp/B01GATSH9U/ref=sr_1_8?s=pc&ie=UTF8&qid=1488550816&sr=1-8&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/EVGA-GeForce-Profile-Graphics-02G-P3-3733-KR/dp/B00L5GZG5C/ref=sr_1_9?s=pc&ie=UTF8&qid=1488550816&sr=1-9&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/EVGA-GeForce-Dual-Link-Graphics-02G-P4-3751-KR/dp/B00IDG3PRI/ref=sr_1_11?s=pc&ie=UTF8&qid=1488550816&sr=1-11&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/ZOTAC-GeForce-Extreme-Graphic-ZT-P10800B-10P/dp/B01GCAVSIO/ref=sr_1_14?s=pc&ie=UTF8&qid=1488550816&sr=1-14&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/MSI-GTX-1080-GAMING-3DisplayPort/dp/B01GLYD7MG/ref=sr_1_16?s=pc&ie=UTF8&qid=1488550816&sr=1-16&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/EVGA-GeForce-Cooling-Graphics-06G-P4-1996-KR/dp/B00Z0UX8TA/ref=sr_1_19?s=pc&ie=UTF8&qid=1488550816&sr=1-19&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/Microtel-Computer-AMTI7018-Cooling-7200RPM/dp/B004YRCYLI/ref=sr_1_20?s=pc&ie=UTF8&qid=1488550816&sr=1-20&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/EVGA-GeForce-GAMING-Graphics-12G-P4-2990-KR/dp/B00UVN21RQ/ref=sr_1_21?s=pc&ie=UTF8&qid=1488550816&sr=1-21&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/ZOTAC-GeForce-GTX-970-ZT-90101-10P/dp/B00NNXVPS2/ref=sr_1_27?s=pc&ie=UTF8&qid=1488552457&sr=1-27&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/Gigabyte-GeForce-Gaming-GDDR5-Graphics/dp/B00NH5T1MS/ref=sr_1_28?s=pc&ie=UTF8&qid=1488552457&sr=1-28&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011")
+
+#FULL LIST
+producturls<-c("https://www.amazon.com/EVGA-GeForce-Support-Graphics-08G-P4-6173-KR/dp/B01GX5YWAO/ref=sr_1_1?s=pc&ie=UTF8&qid=1488550816&sr=1-1&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/EVGA-GeForce-Support-Graphics-08G-P4-6286-KR/dp/B01GAI64GO/ref=sr_1_2?s=pc&ie=UTF8&qid=1488550816&sr=1-2&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/ASUS-GeForce-STRIX-Graphic-STRIX-GTX1070-O8G-GAMING/dp/B01HDUVJ1I/ref=sr_1_3?s=pc&ie=UTF8&qid=1488550816&sr=1-3&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/MSI-GAMING-GTX-1070-8G/dp/B01GXOX3SW/ref=sr_1_4?s=pc&ie=UTF8&qid=1488550816&sr=1-4&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/Gigabyte-GeForce-Graphics-GV-N1070G1-GAMING-8GD/dp/B01H0WU884/ref=sr_1_5?s=pc&ie=UTF8&qid=1488550816&sr=1-5&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/ZOTAC-ZT-P10700C-10P-Wraparound-Ultra-wide-PowerBoost/dp/B01GVNNOSY/ref=sr_1_6?s=pc&ie=UTF8&qid=1488550816&sr=1-6&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/ASUS-GeForce-STRIX-Graphics-STRIX-GTX1080-8G-GAMING/dp/B01GRRRW0A/ref=sr_1_7?s=pc&ie=UTF8&qid=1488550816&sr=1-7&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/NVIDIA-GEFORCE-GTX-1080-Founders/dp/B01GATSH9U/ref=sr_1_8?s=pc&ie=UTF8&qid=1488550816&sr=1-8&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/EVGA-GeForce-Profile-Graphics-02G-P3-3733-KR/dp/B00L5GZG5C/ref=sr_1_9?s=pc&ie=UTF8&qid=1488550816&sr=1-9&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/ViewMax-NVIDIA-GeForce-Express-Support/dp/B00WNO327Y/ref=sr_1_10?s=pc&ie=UTF8&qid=1488550816&sr=1-10&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/EVGA-GeForce-Dual-Link-Graphics-02G-P4-3751-KR/dp/B00IDG3PRI/ref=sr_1_11?s=pc&ie=UTF8&qid=1488550816&sr=1-11&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/MSI-GAMING-GTX-970-4G/dp/B00NN0GEXQ/ref=sr_1_12?s=pc&ie=UTF8&qid=1488550816&sr=1-12&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/GeForce-Express-128-bit-Graphics-GTX750TI-OC-2GD5/dp/B00IB9P1KG/ref=sr_1_13?s=pc&ie=UTF8&qid=1488550816&sr=1-13&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/ZOTAC-GeForce-Extreme-Graphic-ZT-P10800B-10P/dp/B01GCAVSIO/ref=sr_1_14?s=pc&ie=UTF8&qid=1488550816&sr=1-14&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/EVGA-GeForce-Cooling-Graphics-02G-P4-2958-KR/dp/B013WQC9U2/ref=sr_1_15?s=pc&ie=UTF8&qid=1488550816&sr=1-15&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/MSI-GTX-1080-GAMING-3DisplayPort/dp/B01GLYD7MG/ref=sr_1_16?s=pc&ie=UTF8&qid=1488550816&sr=1-16&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/EVGA-SuperClocked-Dual-Link-Graphics-06G-P4-2791-KR/dp/B00BL8BX7O/ref=sr_1_17?s=pc&ie=UTF8&qid=1488550816&sr=1-17&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/ZOTAC-GeForce-DisplayPort-Graphics-ZT-70605-10M/dp/B00M27TSJS/ref=sr_1_18?s=pc&ie=UTF8&qid=1488550816&sr=1-18&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/EVGA-GeForce-Cooling-Graphics-06G-P4-1996-KR/dp/B00Z0UX8TA/ref=sr_1_19?s=pc&ie=UTF8&qid=1488550816&sr=1-19&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/Microtel-Computer-AMTI7018-Cooling-7200RPM/dp/B004YRCYLI/ref=sr_1_20?s=pc&ie=UTF8&qid=1488550816&sr=1-20&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/EVGA-GeForce-GAMING-Graphics-12G-P4-2990-KR/dp/B00UVN21RQ/ref=sr_1_21?s=pc&ie=UTF8&qid=1488550816&sr=1-21&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/EVGA-GeForce-Whisper-Graphics-02G-P4-2966-KR/dp/B00SL2TQ2C/ref=sr_1_22?s=pc&ie=UTF8&qid=1488550816&sr=1-22&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/GeForce-Overclocked-256-bit-DisplayPort-Graphics/dp/B00NJ9BJ8G/ref=sr_1_23?s=pc&ie=UTF8&qid=1488550816&sr=1-23&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/EVGA-GeForce-Quieter-Graphics-04G-P4-2983-KR/dp/B00NT9UT3M/ref=sr_1_24?s=pc&ie=UTF8&qid=1488550816&sr=1-24&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/Nvidia-GeForce-GTX-1070-Founders/dp/B01H1BLHYS/ref=sr_1_25?s=pc&ie=UTF8&qid=1488552457&sr=1-25&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/Asus-TURBO-GTX970-OC-4GD5-ASUS-Graphics-Cards/dp/B00WHJEK1M/ref=sr_1_26?s=pc&ie=UTF8&qid=1488552457&sr=1-26&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/ZOTAC-GeForce-GTX-970-ZT-90101-10P/dp/B00NNXVPS2/ref=sr_1_27?s=pc&ie=UTF8&qid=1488552457&sr=1-27&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011",
+               "https://www.amazon.com/Gigabyte-GeForce-Gaming-GDDR5-Graphics/dp/B00NH5T1MS/ref=sr_1_28?s=pc&ie=UTF8&qid=1488552457&sr=1-28&keywords=gtx+1060&refinements=p_n_feature_keywords_five_browse-bin%3A6147187011")
+
+
+url<-"https://www.amazon.com/s/ref=sr_nr_n_0?fst=as%3Aoff&rh=n%3A172282%2Cn%3A284822%2Ck%3Agpus%2Cp_89%3ANVIDIA&keywords=gpus&ie=UTF8&qid=1488557148&rnid=493964"
+scraping<-read_html(url) 
+links<-html_attr(html_nodes(scraping, "a"), "href")
+producturls<-links[grep("https://www.amazon.com",links)]
+
+producturls<-c("https://www.amazon.com/NVIDIA-GDDR5-ACCELERATOR-PROCESSING-900-52405-0000-000/dp/B01F2P4GBQ/ref=sr_1_1?s=pc&ie=UTF8&qid=1488557342&sr=1-1&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/HP-Tesla-K20X-Graphic-Card/dp/B00AM0FD00/ref=sr_1_2?s=pc&ie=UTF8&qid=1488557342&sr=1-2&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/nVidia-S870-External-Computing-Server/dp/B005GVUTEA/ref=sr_1_3?s=pc&ie=UTF8&qid=1488557342&sr=1-3&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/nVidia-Tesla-M2090-PCI-e-653974-001/dp/B008FQPVZ0/ref=sr_1_4?s=pc&ie=UTF8&qid=1488557342&sr=1-4&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-Tesla-Kepler-Graphics-900-22055-0120-010/dp/B01EM7Q1G8/ref=sr_1_5?s=pc&ie=UTF8&qid=1488557342&sr=1-5&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-Tesla-M2090-Gpu-Card/dp/B005TJKPWU/ref=sr_1_6?s=pc&ie=UTF8&qid=1488557342&sr=1-6&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/NVIDIA-GeForce-Pascal-GDDR5X-900-1G611-2500-000/dp/B01JLKP3IS/ref=sr_1_7?s=pc&ie=UTF8&qid=1488557342&sr=1-7&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-Tesla-M1060-Processing-Processor/dp/B00JMRW0Z0/ref=sr_1_8?s=pc&ie=UTF8&qid=1488557342&sr=1-8&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/NVIDIA-Tesla-Graphic-Card-900-22081-2250-000/dp/B00KDRRTB8/ref=sr_1_9?s=pc&ie=UTF8&qid=1488557342&sr=1-9&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/NVIDIA-M1060-Passive-Computing-PCI-Express/dp/B017O2DCP6/ref=sr_1_11?s=pc&ie=UTF8&qid=1488557342&sr=1-11&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/nVidia-Tesla-Kelper-B3M66A-688982-001/dp/B00BSD4TOG/ref=sr_1_14?s=pc&ie=UTF8&qid=1488557342&sr=1-14&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/616079-001-NVIDIA-graphics-processing-full-height/dp/B008H65MY8/ref=sr_1_15?s=pc&ie=UTF8&qid=1488557342&sr=1-15&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-Gaming-Kepler-Graphics-900-12400-0010-000/dp/B00MH2MFAW/ref=sr_1_17?s=pc&ie=UTF8&qid=1488557342&sr=1-17&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-Gaming-Kepler-Graphics-900-12055-0020-000/dp/B00MGZRYO2/ref=sr_1_18?s=pc&ie=UTF8&qid=1488557342&sr=1-18&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-TESLA-Accelerator-Processing-900-2G600-0000-000/dp/B01MDNO5BK/ref=sr_1_19?s=pc&ie=UTF8&qid=1488557342&sr=1-19&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-Quadro-K3000M-Mobile-N14E-Q1-A2/dp/B015DC6PSK/ref=sr_1_22?s=pc&ie=UTF8&qid=1488557342&sr=1-22&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/NVIDIA-E5Z76AT-Graphics-Card/dp/B00JGOMHXO/ref=sr_1_28?s=pc&ie=UTF8&qid=1488557574&sr=1-28&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/616078-001-NVIDIA-Quadro-6000-graphics/dp/B008FQQ4MY/ref=sr_1_29?s=pc&ie=UTF8&qid=1488557574&sr=1-29&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/TECHNOLOGIES-DisplayPort-Graphics-Adapter-NVIDIA/dp/B00406NS0Y/ref=sr_1_32?s=pc&ie=UTF8&qid=1488557574&sr=1-32&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/671138-001-NVIDIA-Quadro-5000-graphics/dp/B008H65IQ0/ref=sr_1_33?s=pc&ie=UTF8&qid=1488557574&sr=1-33&keywords=gpus&refinements=p_89%3ANVIDIA")
---
+++ Pricing_Data/Amazon Price Scrapping CODE/Amazon Price Scrapping.R
@@ -0,0 +1,106 @@
+## Amazon prices scraping:
+#http://stackoverflow.com/questions/1500365/how-does-one-find-prices-from-amazons-site-programmatically
+library(rvest)
+#searches:
+#https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=nvidia+grapgics+card&rh=i%3Aaps%2Ck%3Anvidia+grapgics+card
+
+#function to grab price data from amazon
+amazonpricegrab<- function(url){
+        scraping<-read_html(url) #
+        price<-scraping %>%
+                html_nodes("#priceblock_ourprice") %>%
+                html_text()
+        
+        name<-scraping %>%
+                html_nodes("#productTitle") %>%
+                html_text()
+        
+        #grab the price of the striked out text
+        #previousprice<-scraping %>%
+        #        html_nodes("#class.a-text-strike") %>%
+        #        html_text()
+        #remove "\n         "
+        #name<-gsub(" ","",name)
+        name<-gsub(pattern="\n         ",replacement="",name)
+        name<-sub(pattern="                    ","",name)
+        name<-sub(pattern="      \n      ","",name)
+        return(data.frame(name,price,Sys.time()))
+}
+
+#List of urls of PRIME products
+producturls<-c("https://www.amazon.com/NVIDIA-GDDR5-ACCELERATOR-PROCESSING-900-52405-0000-000/dp/B01F2P4GBQ/ref=sr_1_1?s=pc&ie=UTF8&qid=1488557342&sr=1-1&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/HP-Tesla-K20X-Graphic-Card/dp/B00AM0FD00/ref=sr_1_2?s=pc&ie=UTF8&qid=1488557342&sr=1-2&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/nVidia-S870-External-Computing-Server/dp/B005GVUTEA/ref=sr_1_3?s=pc&ie=UTF8&qid=1488557342&sr=1-3&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/nVidia-Tesla-M2090-PCI-e-653974-001/dp/B008FQPVZ0/ref=sr_1_4?s=pc&ie=UTF8&qid=1488557342&sr=1-4&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-Tesla-Kepler-Graphics-900-22055-0120-010/dp/B01EM7Q1G8/ref=sr_1_5?s=pc&ie=UTF8&qid=1488557342&sr=1-5&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-Tesla-M2090-Gpu-Card/dp/B005TJKPWU/ref=sr_1_6?s=pc&ie=UTF8&qid=1488557342&sr=1-6&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/NVIDIA-GeForce-Pascal-GDDR5X-900-1G611-2500-000/dp/B01JLKP3IS/ref=sr_1_7?s=pc&ie=UTF8&qid=1488557342&sr=1-7&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-Tesla-M1060-Processing-Processor/dp/B00JMRW0Z0/ref=sr_1_8?s=pc&ie=UTF8&qid=1488557342&sr=1-8&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/NVIDIA-Tesla-Graphic-Card-900-22081-2250-000/dp/B00KDRRTB8/ref=sr_1_9?s=pc&ie=UTF8&qid=1488557342&sr=1-9&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/NVIDIA-M1060-Passive-Computing-PCI-Express/dp/B017O2DCP6/ref=sr_1_11?s=pc&ie=UTF8&qid=1488557342&sr=1-11&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/nVidia-Tesla-Kelper-B3M66A-688982-001/dp/B00BSD4TOG/ref=sr_1_14?s=pc&ie=UTF8&qid=1488557342&sr=1-14&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/616079-001-NVIDIA-graphics-processing-full-height/dp/B008H65MY8/ref=sr_1_15?s=pc&ie=UTF8&qid=1488557342&sr=1-15&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-Gaming-Kepler-Graphics-900-12400-0010-000/dp/B00MH2MFAW/ref=sr_1_17?s=pc&ie=UTF8&qid=1488557342&sr=1-17&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-Gaming-Kepler-Graphics-900-12055-0020-000/dp/B00MGZRYO2/ref=sr_1_18?s=pc&ie=UTF8&qid=1488557342&sr=1-18&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-TESLA-Accelerator-Processing-900-2G600-0000-000/dp/B01MDNO5BK/ref=sr_1_19?s=pc&ie=UTF8&qid=1488557342&sr=1-19&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/Nvidia-Quadro-K3000M-Mobile-N14E-Q1-A2/dp/B015DC6PSK/ref=sr_1_22?s=pc&ie=UTF8&qid=1488557342&sr=1-22&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/NVIDIA-E5Z76AT-Graphics-Card/dp/B00JGOMHXO/ref=sr_1_28?s=pc&ie=UTF8&qid=1488557574&sr=1-28&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/616078-001-NVIDIA-Quadro-6000-graphics/dp/B008FQQ4MY/ref=sr_1_29?s=pc&ie=UTF8&qid=1488557574&sr=1-29&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/TECHNOLOGIES-DisplayPort-Graphics-Adapter-NVIDIA/dp/B00406NS0Y/ref=sr_1_32?s=pc&ie=UTF8&qid=1488557574&sr=1-32&keywords=gpus&refinements=p_89%3ANVIDIA",
+               "https://www.amazon.com/671138-001-NVIDIA-Quadro-5000-graphics/dp/B008H65IQ0/ref=sr_1_33?s=pc&ie=UTF8&qid=1488557574&sr=1-33&keywords=gpus&refinements=p_89%3ANVIDIA")
+
+#Get the price data for all names
+pricedata<-data.frame(NULL)
+for (i in 1:length(producturls)){
+        test<-amazonpricegrab(producturls[i])
+        pricedata<-rbind(pricedata,test)
+}
+
+#Save data- created so we can't overwrite previous day's data (but can overwrite today's data)
+wd<-"/Users/JessicaBohning/Documents/Data Science/Projects/Pricing_Data/Amazon Price Scrapping DATA/Daily Data"
+setwd(wd)
+filename<-paste("Amazon Price Data",Sys.Date(),".csv")
+write.csv(pricedata,file=filename)
+
+#Combine all Saved Data into one file- it is ok to overwrite previously saved data
+#This data will be used for graphing
+files<-list.files(path=wd, full.names=TRUE)
+alldata<-data.frame(NULL)
+len<-c(1:length(files))
+for(i in len){
+        alldata<-rbind(alldata,read.csv(files[i]))
+}
+
+#Eliminate the first column that counts the rows
+alldata<-alldata[2:4]
+#Set column classes (need to remove $ for prices before converting to numeric)
+alldata$name<-as.character(alldata$name)
+alldata$price<-as.character(alldata$price)
+alldata$Sys.time..<-as.POSIXct(alldata$Sys.time..)
+#alldata$Sys.time..<-c(as.Date("3/1/2017","%m/%d/%Y"),as.Date("3/1/2017","%m/%d/%Y"),as.Date("3/1/2017","%m/%d/%Y"),as.Date("3/1/2017","%m/%d/%Y"),as.Date("3/2/2017","%m/%d/%Y"),as.Date("3/2/2017","%m/%d/%Y"),as.Date("3/2/2017","%m/%d/%Y"),as.Date("3/2/2017","%m/%d/%Y"))
+#Remove $ sign in prices & set as.numeric
+alldata$price<-gsub(pattern="[$]","",alldata$price)
+alldata$price<-gsub(pattern="[,]","",alldata$price)
+alldata$price<-as.numeric(alldata$price)
+
+#Create Codes for the names so that the plot is better
+library(ggplot2)
+productnames<-data.frame(NULL)
+productnames<-data.frame(unique(alldata$name))
+productnames$unique.alldata.name.<-as.character(productnames$unique.alldata.name.)
+shortnames<-as.character(1:length(productnames$unique.alldata.name.))
+productnames<-cbind(productnames,shortnames)
+alldatashort<-merge(alldata,productnames,by.x="name",by.y="unique.alldata.name.")
+
+#Save Full Data
+wd<-"/Users/JessicaBohning/Documents/Data Science/Projects/Pricing_Data/Amazon Price Scrapping DATA"
+setwd(wd)
+write.csv(alldatashort,file="ALL DATA.csv")
+
+#Plot Data
+png(filename="timeseriesgraph.png")
+ggplot(alldata,aes(x=alldatashort$Sys.time..,y=alldatashort$price,color=alldatashort$shortnames))+
+        geom_point()+geom_line()+
+        xlab(NULL)+
+        ylab("Price")
+dev.off()
